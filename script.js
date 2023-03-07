7378730f17460858aa3842531f61d21b0f33e5ed
Ruan Leoterio <64181301+ruanleoterio@users.noreply.github.com>
2023-03-07 14:54:49 +0000

    project finish

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 'use strict';
 
+//variables 
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const bntShowModal = document.querySelectorAll('.show-modal')
+
+//events
+for(let i = 0 ; i< bntShowModal.length; ++i)
+    bntShowModal[i].addEventListener('click', () =>{
+
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+   })
+
+
+    const close = function() {
+      modal.classList.add('hidden')
+      overlay.classList.add('hidden')
+    }
+
+    btnCloseModal.addEventListener('click', ()=>{
+      modal.classList.add('hidden')
+      overlay.classList.add('hidden')
+    })
+    overlay.addEventListener('click', ()=>{
+      modal.classList.add('hidden')
+      overlay.classList.add('hidden')
+})
+
+
+
+document.addEventListener('keydown', (e)=>{
+  switch (e.key){
+    case "Enter":
+      console.log('Enter Was Pressed')
+    case "Escape":
+      modal.classList.add('hidden')
+      overlay.classList.add('hidden') 
+
+
+  }
+})
+
+
+    
